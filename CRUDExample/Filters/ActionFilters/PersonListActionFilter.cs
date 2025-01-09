ad22bc41cdaa824054e258c6c0da29d5b560c9f2
健 孙 <1994636224@qq.com>
2025-01-09 15:55:58 +0800

    添加注释

--- CRUDExample/Filters/ActionFilters/PersonListActionFilter.cs
+++ CRUDExample/Filters/ActionFilters/PersonListActionFilter.cs
@@ -6,8 +6,18 @@ namespace CRUDExample.Filters.ActionFilters {
     public class PersonListActionFilter : IActionFilter {
 
         private readonly ILogger<PersonListActionFilter> _logger;
-        private static readonly List<string> searchByOptions = new List<string>() { nameof(PersonResponse.PersonName), nameof(PersonResponse.Email), nameof(PersonResponse.Address), nameof(PersonResponse.Gender), nameof(PersonResponse.DateOfBirth), nameof(PersonResponse.Country), nameof(PersonResponse.Age), nameof(PersonResponse.ReceiveNewsLetters) };
-        private static readonly IDictionary<string, string> searchField = new Dictionary<string, string>() {
+
+        private static readonly List<string> _searchByOptions = new List<string>() {
+            nameof(PersonResponse.PersonName),
+            nameof(PersonResponse.Email),
+            nameof(PersonResponse.Address),
+            nameof(PersonResponse.Gender),
+            nameof(PersonResponse.DateOfBirth),
+            nameof(PersonResponse.Country),
+            nameof(PersonResponse.Age),
+            nameof(PersonResponse.ReceiveNewsLetters)
+        };
+        private static readonly IDictionary<string, string> _searchField = new Dictionary<string, string>() {
                 {"Person Name",nameof(PersonResponse.PersonName) },
                 {"Email", nameof(PersonResponse.Email) },
                 {"Date Of Birth", nameof(PersonResponse.DateOfBirth) },
@@ -16,13 +26,31 @@ namespace CRUDExample.Filters.ActionFilters {
                 {"Address", nameof(PersonResponse.Address) },
                 {"Receive News Letters", nameof(PersonResponse.ReceiveNewsLetters) },
                 {"Age", nameof(PersonResponse.Age) }
-            };
+        };
+
         public PersonListActionFilter(ILogger<PersonListActionFilter> logger) {
             _logger = logger;
         }
+
+        //在进入Controller的Action方法前，此处验证客户端发来的QueryString的合法性，并进行修正
+        public void OnActionExecuting(ActionExecutingContext context) {
+            _logger.LogInformation("{FilterName}.{MethodName}", nameof(PersonListActionFilter), nameof(OnActionExecuting));//Structured Logging
+            context.HttpContext.Items["queryString"] = context.ActionArguments;//因为OnActionExecuted无法访问ActionArguments，所以此处将ActionArguments存储到HttpContext.Items属性中，方便OnActionExecuted访问
+            if(context.ActionArguments.ContainsKey("searchBy")) {//如果QueryString参数有searchBy
+                string? searchBy = Convert.ToString(context.ActionArguments["searchBy"]);//取出searchBy的值
+                if(!string.IsNullOrEmpty(searchBy)) {//如果searchBy的值非空
+                    if(!_searchByOptions.Any(temp => temp == searchBy)) {//且searchBy的值不匹配任意PersonResponse的列名
+                        context.ActionArguments["searchBy"] = _searchByOptions[0];//searchBy修正为PersonName
+                    }
+                }
+            }
+        }
+
+
         public void OnActionExecuted(ActionExecutedContext context) {
             _logger.LogInformation("{FilterName}.{MethodName}", nameof(PersonListActionFilter), nameof(OnActionExecuted));//Structured Logging
             PersonController controller = (PersonController)context.Controller;
+            //由于OnActionExecuted的ActionExecutedContext无法直接访问客户端发来的QueryString，所以需要在OnActionExecuting中把QueryString保存在Items中，OnActionExecuted的ActionExecutedContext可以通过访问Items来间接访问QueryString，QueryString设置到ViewData中，方便View访问
             IDictionary<string, object?>? parameters = context.HttpContext.Items["queryString"] as IDictionary<string, object?>;//取出queryString
             if(parameters != null) {
                 //将特定queryString存放到ViewData中，方便View访问
@@ -39,21 +67,7 @@ namespace CRUDExample.Filters.ActionFilters {
                     controller.ViewData["CurrentSortOrderOption"] = Convert.ToString(parameters["sortOrderOption"]);
                 }
             }
-            controller.ViewData["ColumnNameAndProperty"] = searchField;
-        }
-
-        //在进入Controller的Action方法前，此处验证QueryString参数的合法性，并进行修正
-        public void OnActionExecuting(ActionExecutingContext context) {
-            _logger.LogInformation("{FilterName}.{MethodName}", nameof(PersonListActionFilter), nameof(OnActionExecuting));//Structured Logging
-            context.HttpContext.Items["queryString"] = context.ActionArguments;//因为OnActionExecuted无法访问ActionArguments，所以此处将ActionArguments存储到HttpContext.Items属性中，方便OnActionExecuted访问
-            if(context.ActionArguments.ContainsKey("searchBy")) {//如果QueryString参数有searchBy
-                string? searchBy = Convert.ToString(context.ActionArguments["searchBy"]);//取出searchBy的值
-                if(!string.IsNullOrEmpty(searchBy)) {//如果searchBy的值非空
-                    if(!searchByOptions.Any(temp => temp == searchBy)) {//且searchBy的值不匹配任意PersonResponse的列名
-                        context.ActionArguments["searchBy"] = searchByOptions[0];//searchBy修正为PersonName
-                    }
-                }
-            }
+            controller.ViewData["ColumnNameAndProperty"] = _searchField;
         }
     }
 }
