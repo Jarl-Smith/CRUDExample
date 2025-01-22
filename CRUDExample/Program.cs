85e8c8981dfdd35fc5f83f42b3ec3fc241f2e479
Jarl-Smith <1994636224@qq.com>
2025-01-22 21:55:30 +0800

    添加自定义ExceptionMiddleware

--- CRUDExample/Program.cs
+++ CRUDExample/Program.cs
@@ -10,6 +10,7 @@ using Microsoft.EntityFrameworkCore;
 using Serilog;
 using RepositoryContracts;
 using Repository;
+using CRUDExample.Middleware;
 
 namespace CRUDExample {
     public class Program {
@@ -22,6 +23,8 @@ namespace CRUDExample {
                     .ReadFrom.Configuration(context.Configuration)//read configuration settings from bulid-in configuration
                     .ReadFrom.Services(services);//read out current app's services and make them avaliable to serilog
                 });
+
+            #region 向IoC容器注册组件
             //注册HttpLogging
             builder.Services.AddHttpLogging(
                 options => {
@@ -32,19 +35,26 @@ namespace CRUDExample {
                 option => {
                     option.UseSqlServer(builder.Configuration.GetConnectionString("MySQLServer"));
                 });
+            //注册Controller以及对应的View
             builder.Services.AddControllersWithViews();
-
+            //注册Service
             builder.Services.AddScoped<ICountryRepository, CountryRepository>();
             builder.Services.AddScoped<IPersonRepository, PersonRepository>();
             builder.Services.AddScoped<ICountryService, CountryService>();
             builder.Services.AddScoped<IPersonService, PersonService>();
+            #endregion
 
             var app = builder.Build();
-            app.UseHttpLogging();//开启httplog中间件
+            if(builder.Environment.IsDevelopment()) {//开发环境则使用自带的ExceptionPage
+                app.UseDeveloperExceptionPage();
+            } else {
+                app.UseExceptionHandlingMiddleware();//非开发环境则开启自定义的异常处理中间件
+            }
+            app.UseSerilogRequestLogging();//开启Serilog中间件
             app.UseStaticFiles();//开启静态文件中间件
-            app.UseRouting();
-            app.MapControllers();
-            app.Run();
+            app.UseRouting();//开启路由中间件
+            app.MapControllers();//将路由与Controller建立链接
+            app.Run();//启动程序
         }
     }
 }
